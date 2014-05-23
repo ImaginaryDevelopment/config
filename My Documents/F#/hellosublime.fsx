2880f6e84b34815fa601f56d4dd4589def6d1149
ImaginaryDevelopment <imaginarydevelopment@googlemail>
2014-05-23 15:53:12 -0400

    f# work
    and resharper preferences

--- My Documents/F#/hellosublime.fsx
+++ My Documents/F#/hellosublime.fsx
@@ -2,34 +2,15 @@
 open System.Linq.Expressions
 #r "System.Xml"
 #r "System.Xml.Linq"
-open Microsoft.FSharp.Reflection
-open Microsoft.FSharp.Quotations
-open Microsoft.FSharp.Quotations.Patterns
 open System
 open System.Linq
 open System.Xml
 open System.Xml.Linq
-let rec eval = function
-    | Value(v,t) -> v
-    | Coerce(e,t) -> eval e
-    | NewObject(ci,args) -> ci.Invoke(evalAll args)
-    | NewArray(t,args) -> 
-        let array = Array.CreateInstance(t, args.Length) 
-        args |> List.iteri (fun i arg -> array.SetValue(eval arg, i))
-        box array
-     | NewUnionCase(case,args) -> FSharpValue.MakeUnion(case, evalAll args)
-     | NewRecord(t,args) -> FSharpValue.MakeRecord(t, evalAll args)
-     | NewTuple(args) ->
-         let t = FSharpType.MakeTupleType [|for arg in args -> arg.Type|]
-         FSharpValue.MakeTuple(evalAll args, t)
-     | FieldGet(Some(Value(v,_)),fi) -> fi.GetValue(v)
-     | PropertyGet(None, pi, args) -> pi.GetValue(null, evalAll args)
-     | PropertyGet(Some(x),pi,args) -> pi.GetValue(eval x, evalAll args)
-     | Call(None,mi,args) -> mi.Invoke(null, evalAll args)
-     | Call(Some(x),mi,args) -> mi.Invoke(eval x, evalAll args)
-     | arg -> raise <| NotSupportedException(arg.ToString())
-    and evalAll args = [|for arg in args -> eval arg|]
 
+let Assert b =
+    if not(b) then failwithf "assertion failed"
+let AssertWithF (b:bool, s:string) =
+    if not(b) then failwithf "%s" s
 type String with 
     member x.Before(delimiter:string) : string = 
         x.Substring(0,x.IndexOf(delimiter))
@@ -120,34 +101,37 @@ type System.Xml.XmlNode with
 
 // [<EntryPoint>] // doesn't appear to work properly for sublime build, nor linqpad
 let main args=
-    assert("testing".Before( "ing") = "test")
-    assert ("testing".After("test") = "ing")
-    assert ("testing".BeforeOrSelf("ING") = "testing")
-    assert ("testing".BeforeOrSelf("ng")="testi")
+    printfn "hello %s" ("testing".Before("ing"))
+    Assert("testing".Before("ing") = "test")
+    printfn "hello %s" ("testing".Before("ing"))
+    Assert("testing".Before("ing") = "test")
+    Assert("testing".After("test") = "ing")
+    Assert("testing".BeforeOrSelf("ING") = "testing")
+    Assert("testing".BeforeOrSelf("ng")="testi")
     let dtStart= DateTime(2014,5,22)
     
-    assert ( dtStart.StartOfWeek(DayOfWeek.Monday) = DateTime(2014,5,19))
-    assert ( dtStart.StartOfWeek(DayOfWeek.Tuesday) = DateTime(2014,5,20))
-    assert ( dtStart.StartOfWeek(DayOfWeek.Wednesday) = DateTime(2014,5,21))
-    assert ( dtStart.StartOfWeek(DayOfWeek.Thursday) = DateTime(2014,5,22))
-    assert ( dtStart.StartOfWeek(DayOfWeek.Friday) = DateTime(2014,5,16))
-    assert ( dtStart.StartOfWeek(DayOfWeek.Saturday) = DateTime(2014,5,17))
-    assert ( dtStart.StartOfWeek(DayOfWeek.Sunday) = DateTime(2014,5,18))
-    (
-        let XName n = System.Xml.Linq.XNamespace.None+ n
-        let child = XElement(XName "child")
-        let child2 = XElement(XName "child")
-        let attrib =System.Xml.Linq.XAttribute(XName "test","1")
-        let xmlWithAttrib = System.Xml.Linq.XElement(XName "element",attrib,child,child2)
-        assert( xmlWithAttrib.GetAttribVal(XName "test") = Some("1"))
-        assert( xmlWithAttrib.GetAttribVal(XName "test1") = None)
-        assert( xmlWithAttrib.GetAttribValOrNull(XName "test1") = null)
-        assert( xmlWithAttrib.GetAttribValOrNull(XName "test") = "1")
-        assert(xmlWithAttrib.GetAbsoluteXPath() = "/element")
-        assert(child.GetAbsoluteXPath() = "/element/child[1]")
-        assert(child2.GetAbsoluteXPath() = "/element/child[2]")
-        // attrib.GetAbsoluteXPath().Dump() // not defined, XAttribute is not an XElement
-    )
+    Assert ( dtStart.StartOfWeek(DayOfWeek.Monday) = DateTime(2014,5,19))
+    Assert ( dtStart.StartOfWeek(DayOfWeek.Tuesday) = DateTime(2014,5,20))
+    Assert ( dtStart.StartOfWeek(DayOfWeek.Wednesday) = DateTime(2014,5,21))
+    Assert ( dtStart.StartOfWeek(DayOfWeek.Thursday) = DateTime(2014,5,22))
+    Assert ( dtStart.StartOfWeek(DayOfWeek.Friday) = DateTime(2014,5,16))
+    Assert ( dtStart.StartOfWeek(DayOfWeek.Saturday) = DateTime(2014,5,17))
+    Assert ( dtStart.StartOfWeek(DayOfWeek.Sunday) = DateTime(2014,5,18))
+    
+    let XName n = System.Xml.Linq.XNamespace.None+ n
+    let child = XElement(XName "child")
+    let child2 = XElement(XName "child")
+    let attrib =System.Xml.Linq.XAttribute(XName "test","1")
+    let xmlWithAttrib = System.Xml.Linq.XElement(XName "element",attrib,child,child2)
+    AssertWithF( xmlWithAttrib.GetAttribVal(XName "test") = Some("1"), "Not Some(1)")
+    AssertWithF( xmlWithAttrib.GetAttribVal(XName "test1") = None, "Not None")
+    Assert( xmlWithAttrib.GetAttribValOrNull(XName "test1") = null)
+    Assert( xmlWithAttrib.GetAttribValOrNull(XName "test") = "1")
+    Assert(xmlWithAttrib.GetAbsoluteXPath() = "/element")
+    Assert(child.GetAbsoluteXPath() = "/element/child[1]")
+    Assert(child2.GetAbsoluteXPath() = "/element/child[2]")
+    // attrib.GetAbsoluteXPath().Dump() // not defined, XAttribute is not an XElement
+    
     printfn "%d" 0
     0
     
